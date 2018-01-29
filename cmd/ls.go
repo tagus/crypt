0389e168aedc85c722b3128f03b8f0b0c8cb059e
Sugat Poudel <taguspoudel@gmail.com>
2018-01-28 21:48:46 -0500

    Added ls

--- cmd/ls.go
+++ cmd/ls.go
@@ -1,6 +1,5 @@
 // Copyright © 2018 Sugat Poudel <taguspoudel@gmail.com>
 
-
 package cmd
 
 import (
@@ -12,28 +11,19 @@ import (
 // lsCmd represents the ls command
 var lsCmd = &cobra.Command{
 	Use:   "ls",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ls called")
-	},
+	Short: "List stored services",
+	Long:  `Lists the name of all stored service credentials.`,
+	Run:   ls,
 }
 
 func init() {
 	rootCmd.AddCommand(lsCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// lsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// lsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func ls(cmd *cobra.Command, args []string) {
+	creds := Store.Crypt.Credentials
+	fmt.Printf("You have %d stored credentials.\n", len(creds))
+	for _, v := range creds {
+		fmt.Println(v.Service)
+	}
 }
